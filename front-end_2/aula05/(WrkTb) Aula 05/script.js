bd6c05ec9728a033cbd7f4757d0d80742499e490
Arthur Ferreira <arthurlacerda206@gmail.com>
2021-08-18 16:29:39 -0300

    front2-aula05

---
+++ front-end_2/aula05/(WrkTb) Aula 05/script.js
@@ -0,0 +1,13 @@
+function dark(){
+    let dark= document.querySelector('body');
+    dark.style.backgroundColor = 'black';
+    
+
+}
+
+function white(){
+    let white= document.querySelector('body');
+    white.style.backgroundColor = 'rgb(219, 219, 219)';
+    
+
+}
