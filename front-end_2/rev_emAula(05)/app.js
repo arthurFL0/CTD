a71d7c4d3064b28a8d88c9ba10adac781af322ad
Arthur Ferreira <arthurlacerda206@gmail.com>
2021-08-19 16:45:00 -0300

    aula05/06 revisao e site felinos

---
+++ front-end_2/rev_emAula(05)/app.js
@@ -0,0 +1,11 @@
+
+
+
+let title = document.querySelector('.title');
+
+// vai adicionar uma classe nesse elemento onde tem uma classe 'title
+
+title.classList.add('classuda')
+
+title.style.backgroundColor = 'violet'
+
